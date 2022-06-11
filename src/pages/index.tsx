6a51ece5f403063adf15903ee0d0404d4a8e2bf0
Daniel <danielribeiro.a@outlook.com>
2022-06-10 17:45:02 -0700

    Initital setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Ig.news</title>
+      </Head>
+      <h1>Ignews</h1>
+    </>
+  );
+}
