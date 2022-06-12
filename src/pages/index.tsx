cbd25a7a9ea7680620ecf2b8e2366286d90d023b
Daniel <danielribeiro.a@outlook.com>
2022-06-12 12:58:44 -0700

    finished home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,7 +9,22 @@ export default function Home() {
       <Head>
         <title>Home | Ig.news</title>
       </Head>
-      <h1>Ignews</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome!</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/assets/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
